d75a50a431bf704369c99c15725fb0eaaa83ec60
Melissa Elliston-Boyes <melissa.ellistonboyes@opumtechnologies.com>
2024-09-05 15:56:31 +1200

    Started combining local and provider-managed state

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,11 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/screens/meals.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/filters.dart';
 import 'package:meals/widgets/main_drawer.dart';
 import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/providers/favourites_provider.dart';
+import 'package:meals/providers/filters_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
