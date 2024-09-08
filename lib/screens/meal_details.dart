3ad565fda7ba6903db0d52ba14c9ebdad2d1bc43
Melissa Elliston-Boyes <melissa.ellistonboyes@opumtechnologies.com>
2024-09-08 14:55:52 +1200

    Changed favourite icon to change depending on if meal is a favourite or not

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -14,6 +14,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favouriteMealsProvider);
+
+    final isFavourite = favouriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -30,7 +34,7 @@ class MealDetailsScreen extends ConsumerWidget {
                     : 'Meal removed from favourites.'),
               ));
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
